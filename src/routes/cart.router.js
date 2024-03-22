9a4743a82f8bab00ab3bec8d54e2e1c12b9d2b8d
Martin Beltramo <martin_beltramo@hotmail.com>
2024-03-22 12:37:29 -0300

    Primera pre-entrega - agregado post carrito

--- src/routes/cart.router.js
+++ src/routes/cart.router.js
@@ -11,7 +11,8 @@ router.get('/carts/:id', async (request, response) => {
     const id = parseInt(request.params.id)
     try {
         const carrito = await cartManager.getCarritoById(id)
-        response.send(carrito.products)
+        console.log("carrito", carrito)
+        response.send(carrito)
 
     } catch (error) {
         console.log("Error", error)
@@ -36,5 +37,22 @@ router.post('/carts', async (request, response) => {
     }
 )
 
+router.post('/carts/:id/product/:id_producto', async (request, response) => {
+    const id = parseInt(request.params.id)
+    const id_producto = parseInt(request.params.id_producto)
 
+    let cantidad = parseInt(request.body.cantidad)
+//    if (!cantidad) {
+  //   cantidad = 1
+    //     }
+    try {
+        const actualizarCarrito = await cartManager.agregarProductoAlCarrito(id, id_producto, cantidad)
+        response.send(actualizarCarrito)
+
+
+    } catch (error) {
+        console.log("Error", error)
+        response.send("Error")
+    }
+})
 export default router;
